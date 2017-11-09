7ecbd7c0ee8eba098e5dd3a22c2136c690a8328f
Ionică Bizău <bizauionica@gmail.com>
2017-11-09 20:15:44 +0200

    :arrow_up: 1.0.1 :tada:

--- example/index.js
+++ example/index.js
@@ -1,8 +1,31 @@
 "use strict";
 
+// Usage:
+//// Map
+const emojisMap = require("../lib/map")
+
+if (emojisMap["tada"]) {
+    console.log("Yay, 'tada' exists!")
+    // => Yay, 'tada' exists!
+}
+
+//// List
+const emojisList = require("../lib/list")
+
+console.log(emojisList.filter(c => c.startsWith("heart")))
+// =>
+// [ 'heart',
+//   'heart_decoration',
+//   'heart_eyes',
+//   'heart_eyes_cat',
+//   'heartbeat',
+//   'heartpulse',
+//   'hearts' ]
+
+// Or simply require the lib if you need to use both
 const GitHubApiEmojis = require("../lib");
 
-console.log(GitHubApiEmojis);
+console.log(GitHubApiEmojis.list);
 // =>
 // [ '100',
 //   '1234',
@@ -11,8 +34,17 @@ console.log(GitHubApiEmojis);
 //   'baseball',
 //   ... 1408 more items ]
 
-console.log(GitHubApiEmojis.includes("tada"));
-// => true
-
-console.log(GitHubApiEmojis.includes("foo"));
-// => false
+console.log(GitHubApiEmojis.map);
+// =>
+// { '100': 1,
+//   '1234': 1,
+//   '+1': 1,
+//   '-1': 1,
+//   '1st_place_medal': 1,
+//   '2nd_place_medal': 1,
+//   '3rd_place_medal': 1,
+//   '8ball': 1,
+//   a: 1,
+//   ...
+//   zipper_mouth_face: 1,
+//   zzz: 1 }
--- scripts/build.js
+++ scripts/build.js
@@ -5,5 +5,7 @@ const github = new GitHub()
 
 github.get("emojis", (err, res) => {
     if (err) throw err
-    fs.writeFileSync(`${__dirname}/../lib/index.json`, JSON.stringify(Object.keys(res)))
+    for (let k in res) res[k] = 1
+    fs.writeFileSync(`${__dirname}/../lib/map.json`, JSON.stringify(res))
+    fs.writeFileSync(`${__dirname}/../lib/list.json`, JSON.stringify(Object.keys(res)))
 })
